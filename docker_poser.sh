554c692c40b724bcaed658ba987fc9c32df37062
AustinDoolittle <austindoolittle@gmail.com>
2019-10-20 13:28:05 -0400

    One line fix for all of your problems

--- docker_poser.sh
+++ docker_poser.sh
@@ -4,10 +4,11 @@ set -e
 xhost +
 docker build -t poser -f poser.dockerfile .
 docker run \
+    --privileged \
     -e DISPLAY=$DISPLAY \
     -e QT_X11_NO_MITSHM=1 \
     -v /tmp:/tmp \
-    --device /dev/video1:/dev/video0 \
+    -v /dev/video1:/dev/video0 \
     poser \
     --max-poses 1 \
     --pose-threshold 0.25 \
