03acc461338d133d82608c57a77e3e612d40bcd4
Justin Stanley <justinstanley@berkeley.edu>
2019-12-08 20:34:05 +0000

    IBM Cloud COS addition

--- cloud/docker/cloud_processor.dockerfile
+++ cloud/docker/cloud_processor.dockerfile
@@ -3,10 +3,11 @@ FROM ubuntu
 RUN apt-get update
 RUN apt-get install -y automake autotools-dev g++ git libcurl4-openssl-dev libfuse-dev libssl-dev libxml2-dev make pkg-config python3 python3-pip vim
 
-RUN pip3 install paho-mqtt pymongo
+RUN pip3 install paho-mqtt pymongo ibm-cos-sdk==2.0.1
 
 RUN mkdir /app
+RUN mkdir $HOME/.bluemix
 
 COPY scripts/image_processor.py /app
-
+COPY credentials/.bucket_credentials /root/.bluemix/cos_credentials
 ENTRYPOINT python3 /app/image_processor.py
--- cloud/scripts/image_processor.py
+++ cloud/scripts/image_processor.py
@@ -6,6 +6,9 @@ import uuid
 import json
 import pymongo
 
+import ibm_boto3
+from ibm_botocore.client import Config, ClientError
+
 client_uuid = str(uuid.uuid1())
 
 MQTT_BROKER_IN = 'cloud_broker_1'
@@ -18,9 +21,27 @@ MONGO_DB = 'yoga_poser'
 MONGO_COLLECTION = 'poses'
 
 mongo_client = pymongo.MongoClient('mongodb://{}:{}'.format(MONGO_INSTANCE, MONGO_PORT))
-
 mongo_db = mongo_client[MONGO_DB]
 mongo_collection = mongo_db[MONGO_COLLECTION]
+
+with open('/root/.bluemix/cos_credentials') as f:
+    s3_credentials = json.loads(f.read())
+
+COS_ENDPOINT = 'https://s3.us-south.cloud-object-storage.appdomain.cloud' 
+COS_API_KEY_ID = s3_credentials['apikey']
+COS_AUTH_ENDPOINT = "https://iam.cloud.ibm.com/identity/token"
+COS_RESOURCE_CRN = s3_credentials['resource_instance_id']
+COS_ENDPOINT = 'https://s3.private.us-south.cloud-object-storage.appdomain.cloud'
+COS_BUCKET = 'yoga-poser'
+
+
+cos = ibm_boto3.resource("s3",
+    ibm_api_key_id=COS_API_KEY_ID,
+    ibm_service_instance_id=COS_RESOURCE_CRN,
+    ibm_auth_endpoint=COS_AUTH_ENDPOINT,
+    config=Config(signature_version="oauth"),
+    endpoint_url=COS_ENDPOINT
+    )
 
 
 def on_message(client, userdata, message):
@@ -28,13 +49,21 @@ def on_message(client, userdata, message):
     client_uuid = msg['client']
     rn = dt.datetime.now()
     dt_str = rn.strftime("%Y%m%d_%H%M%f")
-    save_path = '/data/images/{}/{}/{}/{}/'.format(BUCKET_NAME, str(rn.year), str(rn.month), str(rn.day))
+    save_path = '/data/images/{}/{}/{}/{}'.format(BUCKET_NAME, str(rn.year), str(rn.month), str(rn.day))
     if not os.path.exists(save_path):
         os.makedirs(save_path)
     file_name = 'image_{}_{}.jpg'.format(client_uuid, dt_str)
     full_name = '{}/{}'.format(save_path, file_name)
-    with open(full_name, 'wb') as f_output:
-        f_output.write(base64.b64decode(msg.pop('image')))
+    #with open(full_name, 'wb') as f_output:
+    #    f_output.write(base64.b64decode(msg.pop('image')))
+    
+    try:
+        obj = cos.Object(COS_BUCKET, full_name)
+        obj.put(Body = base64.b64decode(msg.pop('image')) )
+
+    except Exception as e:
+        print(e)
+    
     msg['file_name'] = file_name
     msg['capture_dt'] = dt_str
     mongo_result = mongo_collection.insert_one(msg)
