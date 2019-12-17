53dfbf2dbc7e0c0d6268033bbd5948b6b370d7f8
Stephanie Seward <stephanie.j.seward@berkeley.edu>
2019-12-17 10:03:41 -0500

    Update image_download.py

--- edge/Model/image_download.py
+++ edge/Model/image_download.py
@@ -2,5 +2,5 @@ from google_images_download import google_images_download   #importing the libra
 from chromedriver_py import binary_path
 from selenium import webdriver
 response = google_images_download.googleimagesdownload() 
-arguments = {"keywords":"downward dog AND yoga, dlank AND yoga, bridge AND yoga, <additional terms and variations, and even sanskrit names>","limit":50000,"print_urls":False, 'chromedriver': r"<location of your chromedrive.exe", "format": "jpg", "type": "photo"}   #creating list of arguments
+arguments = {"keywords":"downward dog AND yoga, plank AND yoga, bridge AND yoga, <additional terms and variations, and even sanskrit names>","limit":50000,"print_urls":False, 'chromedriver': r"<location of your chromedrive.exe", "format": "jpg", "type": "photo"}   #creating list of arguments
 paths = response.download(arguments)    #passing the arguments to the function
